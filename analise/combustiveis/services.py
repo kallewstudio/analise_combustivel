e9e814c8af57d168c6aed0af323953f81fbcdc73
kallewstudio <kallewstudio@live.com>
2024-10-30 11:02:55 -0300

    Esta filtrando por produto

--- analise/combustiveis/services.py
+++ analise/combustiveis/services.py
@@ -5,13 +5,13 @@ from pandas.core.interchange.dataframe_protocol import DataFrame
 
 
 class Combustiveis:
-    def __init__(self, combustivel):
-        self.combustivel = combustivel
-
-    def read_data(self):
-        caminho_do_arquivo =  os.path.join(settings.BASE_DIR, 'combustiveis', 'db', 'revendas_lpc_2024-10-20_2024-10-26.xlsx')
+    @staticmethod
+    def read_data():
+        caminho_do_arquivo = os.path.join(settings.BASE_DIR, 'combustiveis', 'db',
+                                          'revendas_lpc_2024-10-20_2024-10-26.xlsx')
         data = pd.read_excel(
             caminho_do_arquivo,
             skiprows=9,
-                             )
+            nrows=100,
+        )
         return data
--- analise/combustiveis/urls.py
+++ analise/combustiveis/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('combustiveis/', views.combustiveis, name='combustiveis'),
+    path('combustiveis/produto/<str:produto>/', views.combustiveis, name='filtrar_combustivel'),
 ]
