ec86eeb944615304201bf09fdfcb0f9719d30f88
kallewstudio <kallewstudio@live.com>
2024-10-30 14:17:56 -0300

    Arquivo tratado, salvo na pasta e abre um html com os dados após.

--- analise/combustiveis/urls.py
+++ analise/combustiveis/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('tratar_dados/', views.upload_file, name='upload_file'),
+    path('exibir_dados/<str:nome_arquivo>/', views.exibir_dados, name='exibir_dados'),
 ]
